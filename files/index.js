60becf309af5d220d84d7ce4720e7dceebceb21a
Bharti Devi <2002400.cse.cec@cgc.edu.in>
2024-03-18 23:39:02 +0530

    update

---
+++ files/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const axios = require('axios');
+const cors = require('cors');
+const users = require('./src/user');
+
+require('dotenv').config()
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+///Logger Middleware is defined here
+const loggerMiddleware =(req,res,next)=>{
+  next();
+}
+app.use(loggerMiddleware)
+
+///Routes are defined here
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/api/users', loggerMiddleware, users.getUsers); // using to get all users
+
+///Server is started here 
+app.listen(process.env.port, ()=>{
+    console.log("Listening port...", process.env.port)
+});
